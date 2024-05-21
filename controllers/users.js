041c909d66acfc6aa00d3d30e743fdadf1756d85
sowweggod <rrrrr1111111111@inbox.ru>
2024-05-21 16:08:21 +0300

    Secret added, models changes

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,65 @@
+const {prisma} = require('../prisma/prisma-client');
+const brypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
+
+
 const login = async(req, res) => {
-    res.send('login');
+    const (email, password) = req.body;
+
+    if (!email && !password) {
+        return res.status(400).json({message: 'Пожалуйста, заполните обязательные поля'})
+    }
+
+    const user = await prisma.user.findFirst({
+        where{
+            email,
+        }
+    });
+
+    const isPasswordCorrect = user && {await brypt.compare(password, user.password)};
+
+    if (user && isPasswordCorrect){
+        res.status(200).json({
+            id: user.id,
+            email: user.email,
+            name: user.name,
+        })
+    } else{
+        return res.status(400).json({message: 'Неверно введён логин или пароль'})
+    }
 }
 
 const register = async(req, res) => {
-    res.send('register');
+    const {email, password, name} = req.body;
+
+    if (!email && !password && !name){
+        return res.send(400).json({message: 'Пожалуйста, заполните обязательные поля'})
+    }
+
+    const registeredUser = await prisma.user.findFirst({
+        where: {
+            email
+        }
+
+    });
+
+    
+    if (registeredUser){
+        return res.status(400).json({message: 'Пользователь с таким email уже существует'})
+    }
+
+    const salt = await brypt.genSalt(10);
+    const hashedPassword = await brypt.hash(password, salt);
+
+    const user = await prisma.user.create({
+        data: {
+            email,
+            name,
+            hashedPassword
+        }
+    });
+
+    const secret = process.env.jwt
 }
 
 const current = async(req, res) => {
