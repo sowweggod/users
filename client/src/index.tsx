5d668780707411cdc5e039502a6c64265954868c
Polina Rozova <125026489+sowweggod@users.noreply.github.com>
2024-05-22 20:58:34 +0300

    Inex remastered

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,22 +1,60 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import reportWebVitals from "./reportWebVitals";
+import { ConfigProvider, theme } from "antd";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
 
-const container = document.getElementById('root')!;
+const router = createBrowserRouter([
+  {
+    path: Paths.home,
+    element: <Employees />,
+  },
+  {
+    path: Paths.login,
+    element: <Login />,
+  },
+  {
+    path: Paths.register,
+    element: <Register />,
+  },
+  {
+    path: Paths.employeeAdd,
+    element: <AddEmployee />,
+  },
+  {
+    path: `${Paths.employee}/:id`,
+    element: <Employee />,
+  },
+  {
+    path: `${Paths.employeeEdit}/:id`,
+    element: <EditEmployee />,
+  },
+  {
+    path: `${Paths.status}/:status`,
+    element: <Status />,
+  },
+]);
+
+const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <h1>Hello</h1>
+      <ConfigProvider
+        theme={{
+          algorithm: theme.darkAlgorithm,
+        }}
+      >
+        <Auth>
+          <RouterProvider router={router} />
+        </Auth>
+      </ConfigProvider>
     </Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
